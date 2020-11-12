7dfd141ffbfbc37d951ed4694948a5e0849bb0e3
FVukmirovic <fvukmirovic@gmail.com>
2020-11-12 19:41:38 +0100

    Testiranje Githuba
    Test 123

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Testiramo ReactJS aplikaciju</h1>
+        <h3>Testiranje githuba</h3>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
