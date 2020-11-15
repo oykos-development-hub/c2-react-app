ddfb6a31414d4aee0ee9e33269ea7fd2d9b74f52
Nikola_Gluscevic <66363345+Glus98@users.noreply.github.com>
2020-11-15 19:38:47 +0100

    calculator

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import ClassComponent from './classComponent';
+import Row1 from './row1';
+import Row2 from './row2';
 
 function App(props) {
   const kod = (<div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1>Testiramo ReactJS aplikaciju</h1>
-      <h3>{props.param}</h3>
-
-      <ClassComponent
-          nameAttribute="1111"
-      />
-
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>);
+    <div className = "calculator-bcg">
+      <Row1/>
+      <Row2/>
+    </div>
+</div>);
 
   return kod;
 }
 
 export default App;
+<ClassComponent
+nameAttribute="1111"
+/>
